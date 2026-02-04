2056c2731fb2db832f77aee53abc5724d5f1ff03
happypenguin <1511467743@qq.com>
2026-02-05 00:15:32 +0800

    all

--- gen_url.sh
+++ gen_url.sh
@@ -2,16 +2,16 @@
 set -euo pipefail
 
 # for quick test
-python gen_url.py --pattern "data/spot/daily/klines/SYMBOL/1m/" --symbol-glob "*ETHUSDT"
+# python gen_url.py --pattern "data/spot/daily/klines/SYMBOL/1m/" --symbol-glob "*ETHUSDT"
 
 
-# python gen_url.py --pattern "data/spot/daily/klines/SYMBOL/1m/" --symbol-glob "*USDT"
-# python gen_url.py --pattern "data/futures/um/daily/klines/SYMBOL/1m/" --symbol-glob "*USDT"
-# python gen_url.py --pattern "data/futures/um/monthly/fundingRate/SYMBOL/" --symbol-glob "*USDT"
-# python gen_url.py --pattern "data/futures/um/daily/metrics/SYMBOL/" --symbol-glob "*USDT"
-# python gen_url.py --pattern "data/futures/um/daily/indexPriceKlines/SYMBOL/1m/" --symbol-glob "*USDT"
-# python gen_url.py --pattern "data/futures/um/daily/markPriceKlines/SYMBOL/1m/" --symbol-glob "*USDT"
-# python gen_url.py --pattern "data/futures/um/daily/premiumIndexKlines/SYMBOL/1m/" --symbol-glob "*USDT"
+python gen_url.py --pattern "data/spot/daily/klines/SYMBOL/1m/" --symbol-glob "*USDT"
+python gen_url.py --pattern "data/futures/um/daily/klines/SYMBOL/1m/" --symbol-glob "*USDT"
+python gen_url.py --pattern "data/futures/um/monthly/fundingRate/SYMBOL/" --symbol-glob "*USDT"
+python gen_url.py --pattern "data/futures/um/daily/metrics/SYMBOL/" --symbol-glob "*USDT"
+python gen_url.py --pattern "data/futures/um/daily/indexPriceKlines/SYMBOL/1m/" --symbol-glob "*USDT"
+python gen_url.py --pattern "data/futures/um/daily/markPriceKlines/SYMBOL/1m/" --symbol-glob "*USDT"
+python gen_url.py --pattern "data/futures/um/daily/premiumIndexKlines/SYMBOL/1m/" --symbol-glob "*USDT"
 # zip files
 DATE=$(date +%Y%m%d)
 mkdir -p urls
