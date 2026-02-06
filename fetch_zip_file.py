aa731fa73a41021c31f8ba79ce164b9fccb24d5f
bird <1511467743@qq.com>
2026-02-06 21:34:39 +0800

    add fetch

---
+++ fetch_zip_file.py
@@ -0,0 +1,81 @@
+import argparse
+import asyncio
+import os
+import aiohttp
+import aiofiles
+
+
+class ZipDataFetcher:
+    def __init__(self, url_file: str, max_concurrency: int = 500):
+        self.url_file = url_file
+        self.base_url = "https://data.binance.vision/"
+        self.session = aiohttp.ClientSession(base_url=self.base_url)
+        self.retry_limit = 3
+        self.failed_file = "failed.txt"
+        self.sem = asyncio.Semaphore(max_concurrency)
+
+    async def process_one_url(self, url: str):
+        if self.has_downloaded(url):
+            return
+        async with self.sem:
+            await self.download_file(url)
+
+    def get_out_path(self, url: str) -> str:
+        return url.replace("https://", "")
+
+    def has_downloaded(self, url: str):
+        file_path = self.get_out_path(url)
+        return os.path.exists(file_path)
+
+    async def fetch_content(self, url: str):
+        for attempt in range(self.retry_limit):
+            try:
+                g_url = url.replace(self.base_url, "")
+                async with self.session.get(g_url) as response:
+                    response.raise_for_status()
+                    return await response.read()
+            except Exception as e:
+                print(f"Attempt {attempt + 1} failed for {url}: {e}")
+                await asyncio.sleep(2**attempt)
+        await self.log_failed(url)
+        print(f"Failed to fetch {url} after {self.retry_limit} attempts")
+
+    async def log_failed(self, url: str):
+        async with aiofiles.open(self.failed_file, "a", encoding="utf-8") as f:
+            await f.write(f"{url}\n")
+
+    async def download_file(self, url: str):
+        content = await self.fetch_content(url)
+        if content is None:
+            return
+        file_path = self.get_out_path(url)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        async with aiofiles.open(file_path, "wb") as f:
+            await f.write(content)
+
+    async def run(self):
+        async with aiofiles.open(self.url_file, "r", encoding="utf-8") as f:
+            urls = [line.strip() for line in await f.readlines() if line.strip()]
+        print(f"Total URLs to process: {len(urls)}")
+        tasks = [self.process_one_url(url) for url in urls]
+        await asyncio.gather(*tasks)
+        await self.session.close()
+        if not os.path.exists(self.failed_file):
+            os.remove(self.url_file)
+            print(
+                f"All files downloaded successfully. url file {self.url_file} removed."
+            )
+
+
+async def main(args):
+    fetcher = ZipDataFetcher(args.url_file)
+    await fetcher.run()
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url-file")
+    args = parser.parse_args()
+    print(f"Fetching URLs from file: {args.url_file}")
+    asyncio.run(main(args))
---
+++ fetch_zip_file.sh
@@ -0,0 +1,7 @@
+#!usr/bin/env bash
+set -euo pipefail
+
+# 输入为zip文件路径
+ZIP_PATH=$1
+unzip ZIP_PATH
+
